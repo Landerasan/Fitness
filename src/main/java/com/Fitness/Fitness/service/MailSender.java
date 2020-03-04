4aea63294275b224e8070abada927ddb4d7c3642
Landerasan <Landerasan@gmail.com>
2020-03-04 17:36:49 +0300

    Обнови CART

--- src/main/java/com/Fitness/Fitness/service/MailSender.java
+++ src/main/java/com/Fitness/Fitness/service/MailSender.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.mail.MailParseException;
-import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 import org.springframework.mail.javamail.MimeMessageHelper;
